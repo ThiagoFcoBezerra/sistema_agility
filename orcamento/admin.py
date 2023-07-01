de5b32ba77f72f0e1c62c82e8e76095bbf0b117c
Thiago Bezerra <thiagofcobezerra@hotmail.com>
2023-07-01 15:24:59 -0300

    correcao de bugs da pagina de resultado

--- orcamento/admin.py
+++ orcamento/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from orcamento.models import Categoria, Lancamento, Orcamento, Autorizacao
+from orcamento.models import Categoria, Lancamento, Orcamento, Autorizacao, Faturamento
 # Register your models here.
 
 
 admin.site.register(Categoria)
 admin.site.register(Lancamento)
+admin.site.register(Faturamento)
 
 class OrcamentoAdmin(admin.ModelAdmin):
     list_display = ('categoria', 'data_orcamento', 'descricao', 'valor_orc')
